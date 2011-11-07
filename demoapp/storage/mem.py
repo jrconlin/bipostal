c4049db1e467f656c1b7e74a890d2ce1b16e5407
Jeff Balogh <jbalogh@mozilla.com>
2011-11-07 15:04:43 -0800

    adding storage tests

--- demoapp/storage/mem.py
+++ demoapp/storage/mem.py
@@ -4,7 +4,7 @@ class Storage(object):
         self.db = {}
 
     def resolve_alias(self, alias):
-        return self.db[alias]
+        return self.db.get(alias)
 
     def add_alias(self, email, alias):
         self.db[alias] = email
@@ -12,8 +12,9 @@ class Storage(object):
         return alias
 
     def get_aliases(self, email):
-        return self.db.get(email, None)
+        return self.db.get(email, [])
 
     def delete_alias(self, email, alias):
-        del self.db[alias]
-        self.db[email].remove(alias)
+        if alias in self.db:
+            del self.db[alias]
+            self.db[email].remove(alias)
