6c34462c4600a49cfa97bd934b5b5a14cea601b0
Jeff Balogh <jbalogh@mozilla.com>
2011-11-08 15:57:11 -0800

    give the redis commands a namespace

--- demoapp/storage/redis_.py
+++ demoapp/storage/redis_.py
@@ -2,21 +2,23 @@ import redis
 
 
 class Storage(object):
+    aliases = 'aliases:'
+    emails = 'emails:'
 
     def __init__(self, **kw):
         self.redis = redis.Redis(**kw)
 
     def resolve_alias(self, alias):
-        return self.redis.get(alias)
+        return self.redis.get(self.aliases + alias)
 
     def add_alias(self, email, alias):
-        self.redis.set(alias, email)
-        self.redis.rpush(email, alias)
+        self.redis.set(self.aliases + alias, email)
+        self.redis.rpush(self.emails + email, alias)
         return alias
 
     def get_aliases(self, email):
-        return self.redis.lrange(email, 0, -1)
+        return self.redis.lrange(self.emails + email, 0, -1)
 
     def delete_alias(self, email, alias):
-        self.redis.delete(alias)
-        self.redis.lrem(email, alias)
+        self.redis.delete(self.aliases + alias)
+        self.redis.lrem(self.emails + email, alias)
